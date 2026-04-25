d2b598ba86316f3278c3374f16ecd129b3dfad77
aakashkrsingh1 <aakashkrsingh1@gmail.com>
2026-04-25 23:57:59 +0530

    Fix ALLOWED_HOSTS for deployment

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,7 +25,10 @@ SECRET_KEY = 'django-insecure-!ytt66u4*0msaw)%pvh5yiwad5%o3ap(#f5x2*)@&k*p%eoxu*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["replus-backend.onrender.com",
+    "localhost",
+    "127.0.0.1"
+    ]
 
 
 # Application definition
